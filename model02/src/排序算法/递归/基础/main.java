87b351c2c796d9fbf883abf189d5e5e6d6d1a724
jzx <59948023@qq.com>
2021-03-14 16:21:59 +0800

    Java排序算法学习

---
+++ model02/src/排序算法/递归/基础/main.java
@@ -0,0 +1,30 @@
+package 排序算法.递归.基础;
+
+/**
+ * @title: main
+ * @Auther: jzx
+ * @Date: 2021/3/14 15:43
+ */
+public class main {
+
+
+    //递归：方法自己调用自己
+
+
+    //举例子：1到3之间相乘
+    public static void main(String[] args) {
+
+      int a = xiangcheng(1,3);
+        System.out.println(a);
+    }
+    public static int xiangcheng(int start ,int end){
+        //递归方法的出口
+        if (start==end){
+            return end;
+        }
+        //递归方法的调用
+//        每次调用都要缩小范围
+        return start*xiangcheng(start+1,end);
+    }
+
+}
